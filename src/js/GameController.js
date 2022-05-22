f96fbf6bf0c7a8354c288916fd1c8b765af08221
Irina Paukova <63339174+Irina-Paukova@users.noreply.github.com>
2022-05-22 09:48:16 +0300

    setting up html and css plugins

--- src/js/GameController.js
+++ src/js/GameController.js
@@ -1,3 +1,5 @@
+import GamePlay from "./GamePlay.js"
+
 export default class GameController {
   constructor(gamePlay, stateService) {
     this.gamePlay = gamePlay;
@@ -5,7 +7,7 @@ export default class GameController {
   }
 
   init() {
-    gamePlay.drawUi('prairie');
+    this.gamePlay.drawUi("prairie");
     // TODO: add event listeners to gamePlay events
     // TODO: load saved stated from stateService
   }
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
     entry: './src/index.js',
@@ -24,9 +26,22 @@ module.exports = {
 
             //style.css
             {
-                test: /\.(scss|css)$/,
-                use: ['style-loader', 'css-loader'],
+                test: /\.css$/,
+                use: [
+                    MiniCssExtractPlugin.loader, 'css-loader',
+                ],
             },
         ],
-    }
+    },
+
+    plugins: [
+        new HtmlWebpackPlugin ({
+            template: "./src/index.html",
+            filename: "./index.html"
+        }),
+
+        new MiniCssExtractPlugin ({
+            filename: '[name].css'
+        }),
+    ]
 };
