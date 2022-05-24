ee6af451fa5ac886a1a086d5f6aefb1832bfdc8f
Irina Paukova <63339174+Irina-Paukova@users.noreply.github.com>
2022-05-24 09:36:10 +0300

    render field

--- src/js/GameController.js
+++ src/js/GameController.js
@@ -1,5 +1,3 @@
-import GamePlay from "./GamePlay.js"
-
 export default class GameController {
   constructor(gamePlay, stateService) {
     this.gamePlay = gamePlay;
@@ -7,8 +5,7 @@ export default class GameController {
   }
 
   init() {
-    this.gamePlay.drawUi("prairie");
-    
+    this.gamePlay.drawUi('prairie');
     // TODO: add event listeners to gamePlay events
     // TODO: load saved stated from stateService
   }
